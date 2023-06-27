68557b83c8e485d22dea248bef0ffbef9fba8835
wat <wat>
2023-06-27 10:44:42 -0700

    first

---
+++ ansible-provisioning/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+###################
+# Install ansible #
+if ! grep -q "ansible/ansible" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
+    echo "Adding Ansible PPA"
+    sudo apt-add-repository ppa:ansible/ansible -y
+fi
+
+if ! hash ansible >/dev/null 2>&1; then
+    echo "Installing Ansible..."
+    sudo apt-get update
+    sudo apt-get install software-properties-common ansible git -y
+    sudo apt update
+    sudo apt install python3-pip -y
+else
+    echo "Ansible already installed"
+fi
+
+#####################################
+# Display real installation process #
+ansible-playbook --ask-become-pass -i hosts ansible-desktop.yml
+
